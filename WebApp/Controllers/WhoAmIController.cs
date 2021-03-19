1c6aef832571fda833e1ad7d9484e3e7fbd4f93f
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-03-19 20:39:40 +0100

    initial commit

---
+++ WebApp/Controllers/WhoAmIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class UserInfo
+{
+    public string Name { get; set; }
+}
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhoAmIController : ControllerBase
+    {
+        // GET: /<controller>/
+        public UserInfo Index()
+        {
+            var principal = this.User;
+            var userInfo = new UserInfo
+            {
+                Name = principal.Identity.Name
+            };
+            return userInfo;
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        private readonly string CorsPolicy = "appCorsPolicy";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+            services.AddControllers();
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsPolicy,
+                builder =>
+                {
+                    builder.WithOrigins("*".Split(";")).AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseCors(CorsPolicy);
+
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.User.Identity.IsAuthenticated && context.Request.Path != "/signin-oidc")
+                {
+                    await context.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme);
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                var builder = endpoints.MapControllers();
+//                builder.RequireAuthorization();
+            });
+
+
+            app.UseSpa(spa =>
+            {
+                if (env.IsDevelopment())
+                {
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
+                else
+                {
+                    spa.Options.SourcePath = "ClientApp";
+                }
+            });
+
+        }
+    }
+}
