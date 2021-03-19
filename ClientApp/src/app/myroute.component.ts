fd0270564b2b1c0c863666a74d60be310df9c3fb
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-03-19 20:58:20 +0100

    Added an Angular route

---
+++ ClientApp/src/app/myroute.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myroute',
+  templateUrl: './myroute.component.html',
+})
+export class MyRouteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
